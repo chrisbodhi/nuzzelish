a2f391b464fcb3e84e6f269389729b8caad2ce6f
Chris Boette <cboette@gmail.com>
2020-01-13 23:28:29 -0600

    Change how values are passed in controller to template.

--- lib/nuzzelish_web/controllers/page_controller.ex
+++ lib/nuzzelish_web/controllers/page_controller.ex
@@ -3,6 +3,6 @@ defmodule NuzzelishWeb.PageController do
 
   def index(conn, _params) do
     urls = Nuzzelish.Repo.get_latest
-    render(Map.put(conn, :urls, urls), "index.html")
+    render(conn, "index.html", urls: urls)
   end
 end
