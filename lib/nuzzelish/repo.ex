875338868fbca71737093fa6a7a52cd2fa753f20
Chris Boette <cboette@gmail.com>
2020-01-14 21:35:01 -0600

    Limit links by time, but not the last 24 hours yet. Sort by descending number of times shared.

--- lib/nuzzelish/repo.ex
+++ lib/nuzzelish/repo.ex
@@ -6,8 +6,15 @@ defmodule Nuzzelish.Repo do
     adapter: Ecto.Adapters.Postgres
 
   def get_latest do
-    Nuzzelish.Repo.all from l in Nuzzelish.Link,
+    {_, now} = DateTime.now("Etc/UTC")
+    a_day_away = DateTime.add(now, 24 * 60 * 60, :second)
+
+    latest = Nuzzelish.Repo.all from l in Nuzzelish.Link,
       join: m in assoc(l, :members),
+      where: l.updated_at < ^a_day_away,
       preload: [members: m]
+
+    # Sort links by descending number of members who shared
+    latest |> Enum.sort_by(fn(l) -> length(l.members) end, &>=/2)
   end
 end
