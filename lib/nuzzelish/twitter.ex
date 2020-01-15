bb715b582abbbd58cfbf38cff5b17d41128fa63d
Chris Boette <cboette@gmail.com>
2020-01-15 10:57:42 -0600

    Stream URLs from tweets.

--- lib/nuzzelish/twitter.ex
+++ lib/nuzzelish/twitter.ex
@@ -1,16 +1,44 @@
 defmodule Nuzzelish.Twitter do
-  @account System.get_env("TW_ACCOUNT",  "chrisbodhi")
+  @account System.get_env("TW_ACCOUNT", "chrisbodhi")
   @list System.get_env("TW_LIST", "post-normal")
 
   def get_urls do
-    outside_url? = fn(u) -> !String.starts_with?(u, "https://twitter") end
+    get_tweets()
+    |> IO.inspect
+    |> Enum.map(fn(tw) -> %{name: tw.user.screen_name, urls: extract_urls(tw.entities)} end)
+    |> remove_empty()
+  end
 
-    ExTwitter.list_timeline(@list, @account) |>
-      Enum.map(fn(tw) -> %{name: tw.user.screen_name, urls: Enum.map(tw.entities.urls, fn(ent) -> ent.expanded_url end)} end) |>
-      Enum.filter(fn(ds) -> !Enum.empty?(ds.urls) end) |>
-      Enum.map(fn(ds) -> Enum.filter(ds.urls, fn url -> outside_url?.(url) end) end) |>
-      List.flatten
+  def list_members_ids do
+    ExTwitter.list_members(@list, @account)
+    |> Enum.map(&(&1.id_str))
+    |> Enum.join(", ")
+  end
+
+  defp get_tweets do
+    ExTwitter.list_timeline(@list, @account)
+  end
+
+  defp extract_urls(entities) do
+    Enum.map(entities.urls, fn(e) -> e.expanded_url end)
+    |> Enum.filter(fn(url) -> outside_url?(url) end)
+  end
+
+  defp outside_url?(url) do
+    !String.starts_with?(url, "https://twitter")
+  end
+
+  defp remove_empty(maps) do
+    Enum.filter(maps, fn(m) -> !Enum.empty?(m.urls) end)
+  end
+
+  def stream_from_list(ids_to_follow) do
+    stream = ExTwitter.stream_filter([follow: ids_to_follow], :infinity)
+      |> Stream.map(fn(tw) -> %{name: tw.user.screen_name, urls: extract_urls(tw.entities)} end)
+      |> Stream.filter(fn(m) -> length(m.urls) > 0 end)
+      |> Stream.map(fn(ds) -> IO.inspect(ds) end)
+
+    Enum.to_list(stream)
   end
 end
 
-# todo: come up with mock data and write some tests, cowboy.
