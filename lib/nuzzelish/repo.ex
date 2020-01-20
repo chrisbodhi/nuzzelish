6a90d35063ad7cfcf076d62a1c4e110cefe724f6
Chris Boette <cboette@gmail.com>
2020-01-20 16:39:24 -0600

    Save to database.

--- lib/nuzzelish/repo.ex
+++ lib/nuzzelish/repo.ex
@@ -1,6 +1,7 @@
 import Ecto.Query
 
 defmodule Nuzzelish.Repo do
+  alias Nuzzelish.{Link, Member, Status, Unfurl}
   use Ecto.Repo,
     otp_app: :nuzzelish,
     adapter: Ecto.Adapters.Postgres
@@ -9,7 +10,7 @@ defmodule Nuzzelish.Repo do
     {_, now} = DateTime.now("Etc/UTC")
     a_day_away = DateTime.add(now, 24 * 60 * 60, :second)
 
-    latest = Nuzzelish.Repo.all from l in Nuzzelish.Link,
+    latest = Nuzzelish.Repo.all from l in Link,
       join: m in assoc(l, :members),
       where: l.updated_at < ^a_day_away,
       preload: [members: m]
@@ -18,14 +19,36 @@ defmodule Nuzzelish.Repo do
     latest |> Enum.sort_by(fn(l) -> length(l.members) end, &>=/2)
   end
 
+  def get_or_set_member(member) do
+    member_record = Nuzzelish.Repo.get_by(Member, tw_user_id: member.tw_user_id)
+
+    case member_record do
+      nil -> Nuzzelish.Repo.insert!(member)
+      _ -> member_record
+    end
+  end
+
+  def link_member_changeset(link, member_record) do
+    link_changeset = Ecto.Changeset.change(link)
+    link_changeset |> Ecto.Changeset.put_assoc(:members, [member_record])
+  end
+
+  def link_status_changeset(link, status_record) do
+    link_changeset = Ecto.Changeset.change(link)
+    link_changeset |> Ecto.Changeset.put_assoc(:statuses, [status_record])
+  end
+
   def save_to_db(ds) do
-    %{member: member, links: links} = ds
-    # todo: what does insert do for existing record?
-    member = Nuzzelish.Repo.insert!(member)
-    # todo: map over links array, do for each
-    links = Nuzzelish.Repo.preload(links, [:members])
-    link_changeset = Ecto.Changeset.change(links)
-    l_m_changeset = link_changeset |> Ecto.Changeset.put_assoc(:members, [member])
-    Nuzzelish.Repo.update!(l_m_changeset)
+    %{member: member, links: links, status_id: status_id} = ds
+    member_record = get_or_set_member(member)
+    for link <- links do
+      populated = Unfurl.build(link) |> Nuzzelish.Repo.insert!
+      status = %Status{tw_status_id: status_id} |> Nuzzelish.Repo.insert!
+      populated = Nuzzelish.Repo.preload(populated, [:members, :statuses])
+      lm_changeset = link_member_changeset(populated, member_record)
+      ls_changeset = link_status_changeset(populated, status)
+      Nuzzelish.Repo.update!(lm_changeset)
+      Nuzzelish.Repo.update!(ls_changeset)
+    end
   end
 end
